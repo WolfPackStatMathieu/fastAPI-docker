35c7d7a05d993337a6d69372f56973d8c8d29e86
mthomassin <mathieu.thomassin@eleve.ensai.fr>
2024-06-12 09:33:36 +0000

    pylint

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+"""
+lance une API minimale
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """endpoint racine
+
+    Returns:
+        _type_: _description_
+    """
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
+    """API minimale
+
+    Args:
+        item_id (int): _description_
+        q (str, optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
     return {"item_id": item_id, "q": q}
