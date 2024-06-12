e14f834efae07034b69ace7841edec7c314979e3
mthomassin <mathieu.thomassin@eleve.ensai.fr>
2024-06-12 13:08:43 +0000

    debug root path

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 
 app = FastAPI(
     title="une API minimale"
-    #,root_path="/proxy/8000/"
+    , root_path="/proxy/8000"
     )
 
 @app.get("/")
@@ -15,7 +15,7 @@ def read_root():
     Returns:
         _type_: _description_
     """
-    return {"Hello": "Mathieu"}
+    return {"Hello": "root_path=/proxy/8000"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
